5659cf5eb5c3d14bec6b19b5581e795ab8c14134
chunhui_true <chunhui_true@163.com>
2025-03-05 16:46:29 +0800

    编译时指定缓存pkt个数

--- protolens/src/config.rs
+++ protolens/src/config.rs
@@ -1,4 +1,13 @@
-pub const MAX_PKT_BUFF: usize = 32;
+pub const MAX_PKT_BUFF: usize = if cfg!(feature = "pkt_buff_1024") {
+    1024
+} else if cfg!(feature = "pkt_buff_512") {
+    512
+} else if cfg!(feature = "pkt_buff_256") {
+    256
+} else {
+    128
+};
+
 pub const MAX_READ_BUFF: usize = 512;
 
 #[derive(Clone, Debug)]
